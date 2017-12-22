df2028a7c46c44d8f270bf8d4cdf296c5ebe2e03
shurik <stu.shurik@gmail.com>
2017-12-22 17:09:29 +0200

    [refactor] change search component to pure component

--- src/search.jsx
+++ src/search.jsx
@@ -1,29 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleQueryChange = this.handleQueryChange.bind(this);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <label>Search</label>
-        <input
-          type='text'
-          placeholder='Enter github user name'
-          value={this.props.query || ''}
-          onChange={this.handleQueryChange}
-        />
-      </React.Fragment>
-    );
-  }
-
-  handleQueryChange({ target: { value } }) {
-    this.props.onQuerySubmit(value);
-  }
-}
-
-export default Search;
+export default ({query, onQuerySubmit}) => (
+  <React.Fragment>
+  <label>Search</label>
+  <input
+    type='text'
+    placeholder='Enter github user name'
+    value={query || ''}
+    onChange={({ target: { value } }) => onQuerySubmit(value)}
+  />
+</React.Fragment>
+)
